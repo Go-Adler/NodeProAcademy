40802d4884a664e91862c54a2b737785bb18aff2
Go-Adler <gokul_adler@outlook.com>
2023-01-16 20:41:07 +0530

    start

---
+++ app.js
@@ -0,0 +1,12 @@
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question("Enter your name:", (name) => {
+    console.log("Your name: " + name);
+    rl.close()
+})
+
+rl.on('close')
